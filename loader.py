8c01c8d35ccab6585432946b44e74cecbf54592f
Filyuta Yelena <alena.filiuta@innowise.com>
2026-03-09 13:03:12 +0300

    чтение json вынесено в отдельную функцию

--- loader.py
+++ loader.py
@@ -1,6 +1,4 @@
 import json
-import psycopg2
-import argparse
 from database_conn import Database
 
 
@@ -28,10 +26,12 @@ class DataLoader:
             """)
             self.db.connection.commit()
 
+    def read_json_file(self, file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+
     def load_rooms(self, path_to_rooms):
-        # Загружаем данные о комнатах из JSON-файла
-        with open(args.path_to_rooms, "r", encoding="utf-8") as f:
-            rooms = json.load(f)
+        rooms = self.read_json_file(path_to_rooms)
         with self.db.connection.cursor() as cur:
             for room in rooms:
                 cur.execute(
@@ -45,9 +45,7 @@ class DataLoader:
             self.db.connection.commit()
 
     def load_students(self, path_to_students):
-        # Загружаем данные о cтудентах из JSON-файла
-        with open(args.path_to_students, "r", encoding="utf-8") as f:
-            students = json.load(f)
+        students = self.read_json_file(path_to_students)
         with self.db.connection.cursor() as cur:
             for student in students:
                 cur.execute(
@@ -65,29 +63,3 @@ class DataLoader:
                     ),
                 )
             self.db.connection.commit()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_rooms", type=str, help="путь к файлу rooms.json")
-    parser.add_argument("path_to_students", type=str, help="путь к файлу students.json")
-    parser.add_argument(
-        "format_output", type=str, help="выходной формат данных: xml или json?"
-    )
-    args = parser.parse_args()
-    print("path_to_rooms:", args.path_to_rooms)
-    print("path_to_students:", args.path_to_students)
-
-    # Соединение с базой данных
-    conn = psycopg2.connect(
-        dbname="postgres",
-        user="postgres",
-        password="root",
-        host="localhost",
-        port="5432",
-    )
-    cursor = conn.cursor()
-
-    conn.commit()
-    cursor.close()
-    conn.close()
